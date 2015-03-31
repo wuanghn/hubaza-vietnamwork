57cabeefa9ebe62a5001afe9d1206ade0aa6f4a0
ducanhpham <ppducanh@gmail.com>
2015-03-31 09:48:17 +0700

    update-db

--- app/views/article.blade.php
+++ app/views/article.blade.php
@@ -252,11 +252,11 @@
 
 
 		<!-- jQuery -->
-
+		<script src="public/assets/adapters/jquery.js"></script>
 		<script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
 		<script src="public/assets/js/jquery.js"></script>
 
-		<script src="public/assets/adapters/jquery.js"></script>
+
 		<script src="public/assets/js/ckeditor.js"></script>
 
 		<!-- Bootstrap Core JavaScript -->
